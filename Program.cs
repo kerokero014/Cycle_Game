271fdafc93947077c38827faa852b027d477e5a2
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-11-18 09:03:18 -0700

    commitments

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Unit05.Game.Casting;
+using Unit05.Game.Directing;
+using Unit05.Game.Scripting;
+using Unit05.Game.Services;
+using Unit05.Game;
+
+
+namespace Unit05
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            // create the cast
+            Cast cast = new Cast();
+            Color color1 = new Color(160,32, 240);
+            Color color2 = new Color(54, 243, 255);
+            Point spawn1 = new Point(Constants.MAX_X / 3, Constants.MAX_Y / 2);
+            Point spawn2 = new Point(Constants.MAX_X / 3 * 2, Constants.MAX_Y / 2);
+            cast.AddActor("player1", new Player(color1, spawn1));
+            cast.AddActor("player2", new Player(color2, spawn2));
+
+            // create the services
+            KeyboardService keyboardService = new KeyboardService();
+            VideoService videoService = new VideoService(false);
+           
+            // create the script
+            Script script = new Script();
+            script.AddAction("input", new ControlActors(keyboardService));
+            script.AddAction("update", new MoveActors());
+            script.AddAction("update", new HandleCollisions());
+            script.AddAction("output", new DrawActors(videoService));
+
+            // start the game
+            Director director = new Director(videoService);
+            director.StartGame(cast, script);
+        }
+    }
+}
